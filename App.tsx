77561bb590956a321f017aa31518dfefb9133d08
Julian Camilo <julian20141016@gmail.com>
2024-05-09 18:05:16 -0500

    Created main project

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to TripShare! v3 bien aletosa</Text>
+      <Text>Julian will work on it later 🥵</Text>
+      <Button title="Apretame pues" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
